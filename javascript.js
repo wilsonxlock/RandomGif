787c6bfc0bcfe87cf420502ad71eb1e658c40030
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-09-05 21:34:52 -0400

    giffs rendering and buttons working, need to on click animate and still them

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var philosophers = ["Siddhārtha Gautama", "Friedrich Nietzsche","Alan Watts", "George Orwell", "Aldus Huxly"]
+var philosophers = ["Siddhārtha Gautama", "Friedrich Nietzsche","Alan Watts", "George Orwell", "Aldus Huxley"]
 function renderButtons() {
     $(".buttons").empty();
   for (i=0;i < philosophers.length;i++){
@@ -21,7 +21,35 @@ $("#add-philo").on("click", function(event){
 function createGif(){
     console.log($(this).attr("data-name"))
 }
-$(document).on("click", ".pButton", createGif)
-renderButtons()
+$(document).on("click", ".pButton", createGif, generategif)
 
+renderButtons()
+function generategif(){
+var name = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+name + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .done(function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div class='item'>");
+
+      var rating = results[i].rating;
+
+      var p = $("<p>").text("Rating: " + rating);
+
+      var personImage = $("<img>");
+      personImage.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(personImage);
+
+      $(".gif").prepend(gifDiv);
+    }
+  })}
 })
